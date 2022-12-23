4ac977f52e74de3a2cdf472d2ae64c52f0f0dc1c
odukoyash <sodukoya122@outlook.com>
2022-12-22 19:42:26 -0600

    CONTROL_SCRIPT_TERRAFORM_AWS_S3: Enabling KMS Encryption using Terraform_shina_odukoya_Vers_1.6

--- create_s3_bucket.tf
+++ create_s3_bucket.tf
@@ -4,7 +4,6 @@ resource "aws_s3_bucket" "test" {
   tags		={Environment:	"Development"}
 }
 
-
 #Enabling Server Access Logging
 
 resource "aws_s3_bucket" "log_bucket" {
@@ -21,5 +20,45 @@ resource "aws_s3_bucket_logging" "example" {
 
   target_bucket = aws_s3_bucket.log_bucket.id
   target_prefix = "log/"
+}
+
+
+
+###Set permisions for Acess Control List (ACL)
+
+resource "aws_s3_bucket_acl" "test" {
+  bucket = aws_s3_bucket.test.id
+  acl    = "private"
+}
+
+
+
+
+##Server Side Encryption with AWS_MANAGED KEY
+resource "aws_s3_bucket_server_side_encryption_configuration" "shina-odukoya-encrypt" {
+  bucket = aws_s3_bucket.test.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+##KMS encryption
+resource "aws_kms_key" "demokey" {
+  description             = "key to encrypt bucket objects"
+  deletion_window_in_days = 7
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "kms-shina-odukoya-encrypt" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.demokey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
